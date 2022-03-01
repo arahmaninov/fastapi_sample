42fd34a108b62b6e1e7b44862b671ecbe7796d04
arahmaninov <arahmaninov@yandex.ru>
2022-03-01 18:48:39 +0300

    Added fastapi

--- api/main.py
+++ api/main.py
@@ -1,15 +1,18 @@
-from typing import Optional
+# from typing import Optional
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
