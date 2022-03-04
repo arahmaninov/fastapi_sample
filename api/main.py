42e6064126bd08bc58b8073e01abc3c950bb9271
arahmaninov <arahmaninov@yandex.ru>
2022-03-04 21:22:36 +0300

    Added simple authorization

--- api/main.py
+++ api/main.py
@@ -1,18 +1,16 @@
 # from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
 app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World"}
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Optional[str] = None):
-#     return {"item_id": item_id, "q": q}
+@app.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
